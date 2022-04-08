12bc3f437fbe2528b1bc19f71011545ffa1b1894
linaguz020 <linaguz020@gmail.com>
2022-04-08 11:43:40 -0700

    revert commit to fix main

--- main.cpp
+++ main.cpp
@@ -1,17 +1,6 @@
-#include <iostream>
-#include <string>
+#include "c-echo.h"
+#include "c-count.h"
 
-int main()
-{
-<<<<<<< HEAD
-
-  
-=======
-    std::string name;
-    std::cout << "What is your name?" << std::endl;
-    std::cin >> name;
-    std::cout << "Hello " << name << "!" << std::endl;
-
-    return 0;
->>>>>>> ktran254/add-user-input
+int main(int argv, char** argc) {
+    std::cout << "Count of output: " << count(echo(argv,argc)) << std::endl;
 }
